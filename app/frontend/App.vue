94e0786333fb7a07b1a9a27f5b0ff7c48c8fabcd
Yoko Harada <yokolet@gmail.com>
2024-08-07 22:18:09 +0900

    creates simple Vue page

---
+++ app/frontend/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+
+const message = ref("");
+const messages = ref(['test message1', 'test message2']);
+
+const addNewMessage = () => {
+  console.log(message.value);
+}
+</script>
+
+<template>
+  <form class="" @submit.prevent="addNewMessage">
+    <div class="">
+      <span class="">New Message</span>
+    </div>
+    <input
+        type="text"
+        class=""
+        placeholder="Awesome message"
+        minlength="1"
+        maxlength="50"
+        v-model.trim="message" />
+  </form>
+  <div class="">
+    <ul class="">
+      <li v-for="message in messages">{{ message }}</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
